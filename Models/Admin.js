dfcbb3e11086ba30633d49a408bc9cfb43059944
vB🌎 <pavi.d.h.m.p.777@gmail.com>
2025-12-28 19:00:55 +0530

    Delete Models/Admin.js
    Signed-off-by: :vB🌎 <pavi.d.h.m.p.777@gmail.com>

--- Models/Admin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const AdminSchema = new mongoose.Schema({
-  username: String,
-  password: String
-});
-
-module.exports = mongoose.model("Admin", AdminSchema);
